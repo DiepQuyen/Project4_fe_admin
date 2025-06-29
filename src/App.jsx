8f2860ccf007c16b5abf4b32a1ff31d53401176b
Quyên <147384344+DiepQuyen@users.noreply.github.com>
2025-06-29 22:10:57 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { RouterProvider } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 // project imports
-import './App.css'
+import './app.css'
 import router from 'routes';
 import ThemeCustomization from 'themes';
 
